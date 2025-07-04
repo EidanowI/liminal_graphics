a9f54dcdbe51688b8a261b7f0df3205455695256
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-07-04 13:45:16 +0300

    Create CMake project, add GLFW, GLM and Vulkan lib, add test main.cpp

---
+++ BigScene/main.cpp
@@ -0,0 +1,87 @@
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#ifdef WIN
+#include <Windows.h>
+#endif
+
+#include <iostream>
+#include <vector>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#ifdef WIN
+int main()
+//int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+#elif LINUX
+int main()
+#elif MACOS
+int main()
+#endif
+{
+#ifdef WIN
+    char name[] = "HELLO WIN GLFW";
+#elif LINUX
+    char name[] = "HELLO LINUX";
+#elif MACOS
+    char name[] = "HELLO MACOS";
+#endif
+
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, name, nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    extensionCount++;
+    VkInstance instance;
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        return -1;
+    }
+
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if (deviceCount == 0) {
+        vkDestroyInstance(instance, nullptr);
+        return -1;
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto& device : devices) {
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        std::cout << " - " << deviceProperties.deviceName << std::endl;
+    }
+
+    vkDestroyInstance(instance, nullptr);
+
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = vec * matrix;
+
+    std::cout << test.b;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
