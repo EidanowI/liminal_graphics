4e5590960e83171936d696dad44aab82e545ee56
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-07-13 20:25:26 +0300

    feat: add default pipeline config

--- BigScene/VKPipeline/VKPipeline.h
+++ BigScene/VKPipeline/VKPipeline.h
@@ -9,7 +9,18 @@
 
 
 struct PipelineConfigInfo {
-	
+	VkViewport viewport;
+	VkRect2D scissor;
+	VkPipelineViewportStateCreateInfo viewportInfo;
+	VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+	VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+	VkPipelineMultisampleStateCreateInfo multisampleInfo;
+	VkPipelineColorBlendAttachmentState colorBlendAttachment;
+	VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+	VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+	VkPipelineLayout pipelineLayout = nullptr;
+	VkRenderPass renderPass = nullptr;
+	uint32_t subpass = 0;
 };
 
 
